48d28b33c8867de8382779b1e49484ec375c19f1
Michael Helinckx <82393869+Minkus59@users.noreply.github.com>
2021-10-01 09:26:55 +0200

    Update OvH.inc.php

--- impinfbdd/OvH.inc.php
+++ impinfbdd/OvH.inc.php
@@ -1,7 +1,7 @@
 <?php 
 //$Ovh_serv_bDd = "mysql:host=neurosofxlbase.mysql.db;dbname=neurosofxlbase";
 //$uTil_bDd_serv="neurosofxlbase";
-//$mDp_bDd_serv="Neuro59Soft3";
+//$mDp_bDd_serv="XXXXXX";
 
 $Ovh_serv_bDd = "mysql:host=localhost;dbname=u123974db1";
 $uTil_bDd_serv="u123974db1";
@@ -16,7 +16,7 @@ $Publisher="";
 $Prefix="FS_V1_";
 $KeyGoogleAPI="AIzaSyAGgBKyriKMZxQ0ZEAYNvjqNlCob2rdpuw";
 
-$Adresse="324 rue Jean Jaurès, 59700 Croix";
+$Adresse="324 rue Jean JaurÃ¨s, 59700 Croix";
 $Telephone="06 52 66 06 45";
 
 $GoogleMap='<iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3169.9370902026567!2d3.1568475159413696!3d50.681904478540915!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x47c328f934d0a3ed%3A0x343a66bf2369fd2a!2s324+Rue+Jean+Jaur%C3%A8s%2C+59170+Croix!5e1!3m2!1sfr!2sfr!4v1459864299038" width="100%" height="350" frameborder="0" style="border:0"></iframe>';
